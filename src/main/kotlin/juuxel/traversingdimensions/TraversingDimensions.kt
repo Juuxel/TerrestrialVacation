e8985498805ec6b05fb7d815827cb32d9926af51
Juuxel <6596629+Juuxel@users.noreply.github.com>
2019-12-20 11:05:30 +0200

    Remove climate stuff, use overworld fog function

--- src/main/kotlin/juuxel/traversingdimensions/TraversingDimensions.kt
+++ src/main/kotlin/juuxel/traversingdimensions/TraversingDimensions.kt
@@ -5,11 +5,11 @@ import dev.vatuu.tesseract.api.DimensionRegistry
 import juuxel.traversingdimensions.biome.TDBiomeSource
 import juuxel.traversingdimensions.config.Config
 import juuxel.traversingdimensions.util.visit
-import net.minecraft.SharedConstants
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.math.Vec3d
 import net.minecraft.util.registry.Registry
 import net.minecraft.world.biome.Biome
-import net.minecraft.world.biome.Biomes
 import net.minecraft.world.biome.source.HorizontalVoronoiBiomeAccessType
 import net.minecraft.world.biome.source.VanillaLayeredBiomeSourceConfig
 import net.minecraft.world.gen.chunk.OverworldChunkGenerator
@@ -24,8 +24,6 @@ object TraversingDimensions {
 
     fun init() {
         val config = Config.load()
-
-        SharedConstants.isDevelopment = true
         val biomes = ArrayList<Biome>()
 
         Registry.BIOME.visit { id, biome ->
@@ -40,7 +38,20 @@ object TraversingDimensions {
             BiFunction { world, type ->
                 DimensionBuilder.create()
                     .visibleSky(true)
-                    .fogColour(0xBB, 0xBB, 0xFF)
+                    .fogColour { skyAngle, tickDelta ->
+                        // Copied from OverworldDimension
+
+                        var f = MathHelper.cos(skyAngle * 6.2831855f) * 2.0f + 0.5f
+                        f = MathHelper.clamp(f, 0.0f, 1.0f)
+                        var red = 0.7529412
+                        var green = 0.84705883
+                        var blue = 1.0
+                        red *= f * 0.94 + 0.06
+                        green *= f * 0.94 + 0.06
+                        blue *= f * 0.91 + 0.09
+
+                        Vec3d(red, green, blue)
+                    }
                     .chunkGenerator {
                         OverworldChunkGenerator(
                             it,
