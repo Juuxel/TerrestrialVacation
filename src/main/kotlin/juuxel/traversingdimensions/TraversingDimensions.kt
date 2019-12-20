5da95177a90b89d191561436534ea8db4c86729c
Juuxel <6596629+Juuxel@users.noreply.github.com>
2019-12-20 16:30:14 +0200

    Bye Tesseract

--- src/main/kotlin/juuxel/traversingdimensions/TraversingDimensions.kt
+++ src/main/kotlin/juuxel/traversingdimensions/TraversingDimensions.kt
@@ -1,95 +1,29 @@
 package juuxel.traversingdimensions
 
-import dev.vatuu.tesseract.api.DimensionBuilder
-import dev.vatuu.tesseract.api.DimensionRegistry
-import juuxel.traversingdimensions.biome.TDBiomeSource
 import juuxel.traversingdimensions.config.Config
+import juuxel.traversingdimensions.dimension.TheDimension
 import juuxel.traversingdimensions.util.visit
-import net.fabricmc.fabric.api.registry.CommandRegistry
-import net.minecraft.server.command.CommandManager
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.MathHelper
-import net.minecraft.util.math.Vec3d
 import net.minecraft.util.registry.Registry
-import net.minecraft.world.Heightmap
 import net.minecraft.world.biome.Biome
-import net.minecraft.world.biome.source.HorizontalVoronoiBiomeAccessType
-import net.minecraft.world.biome.source.VanillaLayeredBiomeSourceConfig
-import net.minecraft.world.gen.chunk.OverworldChunkGenerator
-import net.minecraft.world.gen.chunk.OverworldChunkGeneratorConfig
-import java.util.function.BiFunction
-import kotlin.math.max
 
 object TraversingDimensions {
     const val ID: String = "traversing_dimensions"
+
+    internal val biomes: List<Biome> get() = _biomes
+    private val _biomes: MutableList<Biome> = ArrayList()
 
     fun id(path: String): Identifier =
         Identifier(ID, path)
 
     fun init() {
         val config = Config.load()
-        val biomes = ArrayList<Biome>()
-
         Registry.BIOME.visit { id, biome ->
             if ((id.namespace == "traverse" || id.namespace == "terrestria") && id !in config.blacklistedBiomes) {
-                biomes += biome
+                _biomes += biome
             }
         }
 
-        val dimensionType = DimensionRegistry.getInstance().registerDimensionType(
-            id("dimension"),
-            true,
-            BiFunction { world, type ->
-                DimensionBuilder.create()
-                    .visibleSky(true)
-                    .fogColour { skyAngle, tickDelta ->
-                        // Copied from OverworldDimension
-
-                        var f = MathHelper.cos(skyAngle * 6.2831855f) * 2.0f + 0.5f
-                        f = MathHelper.clamp(f, 0.0f, 1.0f)
-                        var red = 0.7529412
-                        var green = 0.84705883
-                        var blue = 1.0
-                        red *= f * 0.94 + 0.06
-                        green *= f * 0.94 + 0.06
-                        blue *= f * 0.91 + 0.09
-
-                        Vec3d(red, green, blue)
-                    }
-                    .chunkGenerator {
-                        OverworldChunkGenerator(
-                            it,
-                            TDBiomeSource(biomes, VanillaLayeredBiomeSourceConfig(it.levelProperties)),
-                            OverworldChunkGeneratorConfig()
-                        )
-                    }
-                    .build(world, type)
-            },
-            HorizontalVoronoiBiomeAccessType.INSTANCE
-        )
-
-        CommandRegistry.INSTANCE.register(false) { dispatcher ->
-            dispatcher.register(
-                CommandManager.literal(ID)
-                    .requires { it.hasPermissionLevel(2) }
-                    .executes {
-                        val player = it.source.player
-                        val world = it.source.world.server.getWorld(dimensionType)
-                        player.teleport(
-                            world,
-                            player.x,
-                            max(
-                                player.y,
-                                world.getTopY(Heightmap.Type.WORLD_SURFACE, player.x.toInt(), player.z.toInt())
-                                    .toDouble()
-                            ),
-                            player.z,
-                            player.yaw,
-                            player.pitch
-                        )
-                        1
-                    }
-            )
-        }
+        TheDimension.init()
     }
 }
---
+++ src/main/kotlin/juuxel/traversingdimensions/dimension/TheDimension.kt
@@ -0,0 +1,43 @@
+package juuxel.traversingdimensions.dimension
+
+import juuxel.traversingdimensions.TraversingDimensions
+import juuxel.traversingdimensions.biome.TDBiomeSource
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensionType
+import net.minecraft.block.pattern.BlockPattern
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.Heightmap
+import net.minecraft.world.World
+import net.minecraft.world.biome.source.HorizontalVoronoiBiomeAccessType
+import net.minecraft.world.biome.source.VanillaLayeredBiomeSourceConfig
+import net.minecraft.world.dimension.DimensionType
+import net.minecraft.world.dimension.OverworldDimension
+import net.minecraft.world.gen.chunk.ChunkGenerator
+import net.minecraft.world.gen.chunk.ChunkGeneratorConfig
+import net.minecraft.world.gen.chunk.OverworldChunkGenerator
+import net.minecraft.world.gen.chunk.OverworldChunkGeneratorConfig
+
+class TheDimension(world: World, private val type: DimensionType) : OverworldDimension(world, type) {
+    override fun getType() = type
+
+    override fun createChunkGenerator(): ChunkGenerator<out ChunkGeneratorConfig> =
+        OverworldChunkGenerator(
+            world,
+            TDBiomeSource(TraversingDimensions.biomes, VanillaLayeredBiomeSourceConfig(world.levelProperties)),
+            OverworldChunkGeneratorConfig()
+        )
+
+    companion object {
+        val TYPE: DimensionType = FabricDimensionType.builder()
+            .defaultPlacer { teleported, destination, _, _, _ ->
+                val pos = Vec3d(teleported.x, destination.getTopY(Heightmap.Type.MOTION_BLOCKING, teleported.blockPos.x, teleported.blockPos.z).toDouble(), teleported.z)
+                BlockPattern.TeleportTarget(pos, Vec3d.ZERO, 0)
+            }
+            .biomeAccessStrategy(HorizontalVoronoiBiomeAccessType.INSTANCE)
+            .factory(::TheDimension)
+            .buildAndRegister(TraversingDimensions.id("dimension"))
+
+        fun init() {
+
+        }
+    }
+}
